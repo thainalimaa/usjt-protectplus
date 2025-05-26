ca0c32dc8dd4ab2486b148308e14662b4c0a847f
thainalimaa <thaina.matos0504@gmail.com>
2025-05-25 23:46:10 -0300

    feat(front): fix pages

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,14 +1,8 @@
+import React from "react";
 import ChatAnalyse from "../components/ChatAnalyse";
-import React from 'react';
 
-
-function Chat() {
-  return (
-    <div style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center", background: "#f1f1f1" }}>
-      <ChatAnalyse />
-    </div>
-
-  );
-}
+const Chat = ({ setCurrentPage }) => {
+  return <ChatAnalyse setCurrentPage={setCurrentPage} />;
+};
 
 export default Chat;
--- src/server/geminiClient.js
+++ src/server/geminiClient.js
@@ -1,4 +1,4 @@
-const API_KEY = process.env.REACT_APP_GEMINI_API_KEY;
+const API_KEY = "";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
 
 export async function analisarMensagem(mensagem) {
